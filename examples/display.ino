fc69236e84b1154343876732b611dacdb21d21d5
maeek <maciej@suchanecki.me>
2021-12-29 14:46:24 +0100

    refactor: Move display to examples

--- examples/display.ino
+++ examples/display.ino
@@ -2,12 +2,12 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH1106.h>
-//definiujemy piny I2C, do&nbsp;których&nbsp;podłączony został wyświetlacz
+
+//I2C Pins
 #define OLED_SDA 21
 #define OLED_SCL 22
 
-
-Adafruit_SH1106 display(21, 22);
+Adafruit_SH1106 display(OLED_SDA, OLED_SCL);
 
 void print_on_screen(String message)
 {
